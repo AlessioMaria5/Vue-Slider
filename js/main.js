0b7066fa71d7ae77b3fb4a165585a304b951b0e4
AlessioMaria5 <alessiomaria5@gmail.com>
2022-07-24 07:07:47 +0200

    setInterval ok

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 const vueApp = new Vue({
 
-    el: '#vueApp',    
+    el: '#vueApp',
+    created:  function(){
+        setInterval(this.activeSliderNext,3000)
+    },
 
     data: {
         index: 0,
         images: [
-
             {
                 url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
                 title: 'Svezia',
@@ -41,69 +43,27 @@ const vueApp = new Vue({
 
         activeSliderPrev(){
 
-            if(this.index <1) {
-                
+            if(this.index <1) {             
                 this.index = this.images.length -1
                }
                else {
-    
                 this.index--
                }
         },
 
         activeSliderNext(){
 
-            if(this.index >3) {
+            this.index++
 
-                this.index = 0
-                
+            if(this.index >4) {
+                this.index = 0      
                }
-               else {
-
-                this.index++
-                
-               }
+               console.log(this.index);         
         },
         
         clicked(indiceFor){
            
                 this.index = indiceFor
             }
-
         }
-    
     })
-
-
-
-// const images = [
-//     {
-//         url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
-//         title: 'Svezia',
-//         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
-//     },
-
-//     {
-//         url: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg',
-//         title: 'Perù',
-//         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
-//     },
-
-//     {
-//         url: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c',
-//         title: 'Chile',
-//         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
-//     },
-//     {
-//         url: 'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg',
-//         title: 'Argentina',
-//         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
-//     },
-//     {
-//         url: 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop',
-//         title: 'Colombia',
-//         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
-//     },
-// ];
-
-
